e9b0f4969fc1954451e09fcf4e360206db5d9c4a
Pedro Nieto <pedronieto.2005@gmail.com>
2024-05-01 22:49:41 -0300

    feat: Initial Commit

---
+++ src/main/java/main/Principal.java
@@ -0,0 +1,15 @@
+package main;
+
+import view.Menu;
+
+/**
+ *
+ * @author pedroo-nietoo
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        Menu janela = new Menu();
+        janela.setVisible(true);
+    }
+}
